5efae78ed1b77a84fd97b32a692647b88926d317
hiiiroaqua <plutonium.090301@gmail.com>
2021-10-21 21:20:42 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <ul>
+    <li v-for="content in contents" :key="content.id">
+      <nuxt-link :to="`/${content.id}`">
+        {{ content.title }}
+      </nuxt-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  async asyncData() {
+    const { data } = await axios.get(
+      'https://blogtutorial.microcms.io/api/v1/blog',
+      {
+        headers: {'X-API-KEY': '9309ffc8-f4c3-4c73-95a4-4a8deef642a7'}
+      }
+    )
+    return data
+  }
+}
+</script>
